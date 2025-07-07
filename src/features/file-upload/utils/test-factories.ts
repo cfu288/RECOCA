16846ae591631f7df5bef0ec1ccf39faf7720a84
Chris Fu <invent288cfu@gmail.com>
2025-07-06 23:31:09 -0400

    fix: fix excel file test (#2)

--- src/features/file-upload/utils/test-factories.ts
+++ src/features/file-upload/utils/test-factories.ts
@@ -1,13 +1,50 @@
 import { ExcelSheet } from "../../../core/data/loaders/excel-loader";
 
+// Create a proper File-like object that works in Node.js
+class MockFile {
+  name: string;
+  type: string;
+  size: number;
+  lastModified: number;
+  
+  constructor(bits: any[], name: string, options?: { type?: string; lastModified?: number }) {
+    this.name = name;
+    this.type = options?.type || '';
+    this.size = bits.reduce((acc, bit) => acc + (bit?.length || 0), 0);
+    this.lastModified = options?.lastModified || Date.now();
+  }
+  
+  // Add minimal File API methods that might be used
+  slice(start?: number, end?: number, contentType?: string): Blob {
+    return {} as Blob;
+  }
+  
+  arrayBuffer(): Promise<ArrayBuffer> {
+    return Promise.resolve(new ArrayBuffer(this.size));
+  }
+  
+  text(): Promise<string> {
+    return Promise.resolve('');
+  }
+  
+  stream(): ReadableStream {
+    return {} as ReadableStream;
+  }
+}
+
+// Polyfill File for Node.js if it doesn't exist
+if (typeof global !== 'undefined' && !global.File) {
+  (global as any).File = MockFile;
+}
+
 export const TestFactories = {
   mockExcelFile: (name = "test.xlsx") =>
-    new File([""], name, {
+    new MockFile([""], name, {
       type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
     }),
 
   mockCsvFile: (name = "test.csv") =>
-    new File([""], name, {
+    new MockFile([""], name, {
       type: "text/csv",
     }),
 
